7e513f267a6b1023631a83c552048a6717371a8e
lufuno01 <104906398+lufuno01@users.noreply.github.com>
2022-05-24 16:18:33 +0200

    Task 0.7

---
+++ task 0.7.py
@@ -0,0 +1,15 @@
+
+
+def convert_fanhrenheit_to_celsius(fahrenheit):
+    celsius= ((fahrenheit -32) * (5/9))
+    return celsius
+
+print(convert_fanhrenheit_to_celsius())
+
+
+def convert_celsius_to_fanhrenheit(celsius):
+    fanhrenheit= ((celsius * 9/5) + 32)
+    
+    return celsius
+
+print(convert_celsius_to_fanhrenheit()) 

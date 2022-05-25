6bb7bb1b5e1ac52ee76fc4f43311c61ed5579299
lufuno01 <104906398+lufuno01@users.noreply.github.com>
2022-05-25 11:44:20 +0200

    task 0.6

--- task 0.6.py
+++ task 0.6.py
@@ -2,12 +2,13 @@
 def finding_the_largest_number(number1,number2,number3):
  if (number1 > number2) and (number1 > number3):
       number = number1
-     
+      return number
  elif (number2 > number3) and (number2 > number1):
       number = number2
-    
+      return number
  else:
      number = number3
      
- print("The highest number is ",number)   
-finding_the_largest_number() 
+     return number
+  
+print(finding_the_largest_number(22,5,27)) 
